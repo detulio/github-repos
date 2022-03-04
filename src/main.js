b7cc7315ab3f8d8e02bf3d5eae0d83d3e617cf9b
l.detulio@rwesistemas.com.br <qwe123!@#>
2022-03-04 11:56:22 -0300

    App code

--- src/main.js
+++ src/main.js
@@ -3,5 +3,54 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css'      //theme
+import 'primevue/resources/primevue.min.css'                 //core css
+import 'primeicons/primeicons.css';                          //icons
+import 'primeflex/primeflex.css'
+createApp(App).use(store).use(router).use(PrimeVue, {
+    locale: {
+        startsWith: 'Inicia com',
+        contains: 'Contem',
+        notContains: 'Não contem',
+        endsWith: 'Termina com',
+        equals: 'Igual',
+        notEquals: 'Diferente',
+        noFilter: 'Sem filtro',
+        lt: 'Menor que',
+        lte: 'Menor ou igual à',
+        gt: 'Maior que',
+        gte: 'Maior ou igual à',
+        dateIs: 'Data é',
+        dateIsNot: 'Data não é',
+        dateBefore: 'Data anterior',
+        dateAfter: 'Data posterior',
+        clear: 'Limpar',
+        apply: 'Aplicar',
+        matchAll: 'Corresponder Tudo',
+        matchAny: 'Corresponder Qualquer',
+        addRule: 'Adicionar Regra',
+        removeRule: 'Remover Regra',
+        accept: 'Sim',
+        reject: 'Não',
+        choose: 'eSCOLHER',
+        upload: 'Upload',
+        cancel: 'Cancelar',
+        dayNames: ["Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"],
+        dayNamesShort: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+        dayNamesMin: ["Do","Se","Te","Qa","Qi","Se","Sa"],
+        monthNames: ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+        monthNamesShort: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun","Jul", "Ago", "Set", "Out", "Nov", "Dez"],
+        today: 'Hoje',
+        weekHeader: 'Sm',
+        firstDayOfWeek: 0,
+        dateFormat: 'dd/mm/yy',
+        weak: 'Semana',
+        medium: 'Médio',
+        strong: 'Forte',
+        passwordPrompt: 'Entre com a senha',
+        emptyFilterMessage: 'Nenhum resultado encontrado',
+        emptyMessage: 'Sem opções'
+    }
+}).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,215 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex justify-content-center align-items-center">
+    <Card v-if="info" style="width: 80vw">
+      <template #title>
+        <div class="flex user-info">
+          <div>
+            <img :src="info.avatar_url" height="50"/>
+          </div>
+          <div class="flex align-items-center login-info">
+            {{ info.login }} -&nbsp;<i><a :href="info.html_url">{{ info.html_url }}</a></i>
+          </div>
+        </div>
+      </template>
+      <template #content>
+        <Card class="flex">
+          <template #content>
+            <div class="flex justify-content-start">
+              <span class="p-float-label">
+                <InputText id="login" type="text" v-model="login" @keydown.enter="serviceGithub" />
+                <label for="login">Login Github</label>
+              </span>
+            </div>
+          </template>
+        </Card>
+        <DataTable :value="info.repos" :paginator="true" class="p-datatable-customers" showGridlines :rows="10"
+                   dataKey="id" v-model:filters="filters" filterDisplay="menu" :loading="loading" responsiveLayout="scroll"
+                   :globalFilterFields="['full_name','description','visibility']">
+          <template #header>
+            <div class="flex justify-content-between">
+              <Button type="button" icon="pi pi-filter-slash" label="Limpar Filtros" class="p-button-outlined" @click="clearFilter()"/>
+              <span class="p-input-icon-left">
+                  <i class="pi pi-search" />
+                  <InputText v-model="filters['global'].value" placeholder="Digite um Nome, Descrição" />
+              </span>
+            </div>
+          </template>
+          <template #empty>
+            Nenhum repositório encontrado.
+          </template>
+          <template #loading>
+            Carregando repositórios. Aguarde...
+          </template>
+          <Column field="full_name" header="Nome" style="min-width:12rem">
+            <template #body="{data}">
+              {{data.full_name}}
+            </template>
+            <template #filter="{filterModel}">
+              <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Busca por nome"/>
+            </template>
+          </Column>
+          <Column field="description" header="Descrição" style="min-width:12rem">
+            <template #body="{data}">
+              {{data.description}}
+            </template>
+            <template #filter="{filterModel}">
+              <InputText type="text" v-model="filterModel.value" class="p-column-filter" placeholder="Busca por descrição"/>
+            </template>
+          </Column>
+          <Column header="Criado em" filterField="created_at" dataType="date" style="min-width:10rem">
+            <template #body="{data}">
+              {{ formatDate(data.created_at) }}
+            </template>
+            <template #filter="{filterModel}">
+              <Calendar v-model="filterModel.value" dateFormat="dd/mm/yy" placeholder="dd/mm/yyyy" />
+            </template>
+          </Column>
+          <Column header="Última Atualização" filterField="pushed_at" dataType="date" style="min-width:10rem">
+            <template #body="{data}">
+              {{formatDate(data.pushed_at)}}
+            </template>
+            <template #filter="{filterModel}">
+              <Calendar v-model="filterModel.value" dateFormat="dd/mm/yy" placeholder="dd/mm/yyyy" />
+            </template>
+          </Column>
+          <Column field="visibility" header="Visibilidade" :filterMenuStyle="{'width':'14rem'}" style="min-width:12rem">
+            <template #body="{data}">
+              {{data.visibility}}
+            </template>
+            <template #filter="{filterModel}">
+              <Dropdown v-model="filterModel.value" :options="visibilities" placeholder="Qualquer" class="p-column-filter" :showClear="true">
+                <template #value="slotProps">
+                  <span :class="'customer-badge status-' + slotProps.value" v-if="slotProps.value">{{slotProps.value}}</span>
+                  <span v-else>{{slotProps.placeholder}}</span>
+                </template>
+                <template #option="slotProps">
+                  <span :class="'customer-badge status-' + slotProps.option">{{slotProps.option}}</span>
+                </template>
+              </Dropdown>
+            </template>
+          </Column>
+          <Column field="archived" header="Arquivado" dataType="boolean" bodyClass="text-center" style="min-width:8rem">
+            <template #body="{data}">
+              <i class="pi" :class="{'true-icon pi-check-circle': data.archived, 'false-icon pi-times-circle': !data.archived}"></i>
+            </template>
+            <template #filter={filterModel}>
+              <TriStateCheckbox v-model="filterModel.value" />
+            </template>
+          </Column>
+          <Column field="has_issue" header="Problemas" dataType="boolean" bodyClass="text-center" style="min-width:8rem">
+            <template #body="{data}">
+              <i class="pi" :class="{'true-icon pi-check-circle': data.has_issue, 'false-icon pi-times-circle': !data.has_issue}"></i>
+            </template>
+            <template #filter={filterModel}>
+              <TriStateCheckbox v-model="filterModel.value" />
+            </template>
+          </Column>
+        </DataTable>
+      </template>
+    </Card>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script setup>
+import Button from "primevue/button"
+import Card from "primevue/card"
+import InputText from 'primevue/inputtext';
+import TriStateCheckbox from 'primevue/tristatecheckbox';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import Calendar from 'primevue/calendar';
+import Dropdown from 'primevue/dropdown';
+import axios from  'axios';
+import {onMounted, ref} from "vue";
+import {FilterMatchMode,FilterOperator} from 'primevue/api';
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+let info = ref(null);
+let login = ref("detulio")
+
+const filters = ref({
+  'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
+  'full_name': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+  'description': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+  'created_at': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.DATE_IS}]},
+  'pushed_at': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.DATE_IS}]},
+  'visibility': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.EQUALS}]},
+  'archived': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.EQUALS}]},
+  'has_issues': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.EQUALS}]},
+});
+
+const visibilities = ref([
+   'public', 'private'
+]);
+
+const formatDate = (value) => {
+  return value.toLocaleDateString('pt-BR', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  });
+};
+
+let loading = ref(false);
+
+const serviceGithub = () => {
+  localStorage.setItem('gitLogin', login.value)
+
+  loading.value = true;
+  axios.get(`https://api.github.com/users/${login.value}`).then(resp => {
+    info.value = resp.data
+    axios.get(`https://api.github.com/users/${login.value}/repos`).then(resp => {
+      info.value.repos = resp.data
+      info.value.repos.forEach(function (v, i){
+        info.value.repos[i].created_at = new Date(v.created_at)
+        info.value.repos[i].pushed_at = new Date(v.pushed_at)
+      })
+      loading.value = false
+    }).catch(() => {
+      loading.value = false
+    }).catch(() => {
+      loading.value = false
+    })
+  });
 }
+
+onMounted(() => {
+  login.value = localStorage.getItem('gitLogin') || 'detulio'
+  serviceGithub()
+})
+
+const clearFilter = () => {
+  initFilters();
+};
+const initFilters = () => {
+  filters.value = {
+    'global': {value: null, matchMode: FilterMatchMode.CONTAINS},
+    'full_name': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+    'description': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.STARTS_WITH}]},
+    'created_at': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.DATE_IS}]},
+    'pushed_at': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.DATE_IS}]},
+    'visibility': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.EQUALS}]},
+    'archived': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.EQUALS}]},
+    'has_issues': {operator: FilterOperator.AND, constraints: [{value: null, matchMode: FilterMatchMode.EQUALS}]},
+  }
+};
+
 </script>
+
+<style scoped>
+  img {
+    border-radius: 50px;
+  }
+
+  .user-info {
+    border: solid 1px lightgray;
+    border-radius: 5px;
+    padding: 10px 5px 5px 10px;
+    box-shadow: lightgray 2px 2px;
+  }
+
+  .login-info {
+    font-family: 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+    font-size: 22px;
+    padding-left: 10px;
+  }
+</style>
